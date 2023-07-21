2d099936a28a479ddb157f5f180b06ed3dcc3633
y1ungspace <y1ung.space@gmail.com>
2023-07-21 20:57:51 +0200

    add new images to Swiper component

--- app/components/home/Swiper.tsx
+++ app/components/home/Swiper.tsx
@@ -14,6 +14,10 @@ import "swiper/css/pagination";
 // import required modules
 import { EffectCoverflow, Pagination } from "swiper";
 
+import swiperSlides from "@/app/lib/swiperSlides";
+
+const arrOfSlides = swiperSlides
+
 const SwiperSlider = () => {
   return (
     <>
@@ -33,37 +37,14 @@ const SwiperSlider = () => {
         modules={[EffectCoverflow]}
         className="mySwiper"
       >
-        <SwiperSlide>
+        {arrOfSlides.map((slide) =>
+          <SwiperSlide>
           <img 
-            src="https://www.dropbox.com/s/3u5g0h3d6sh86xu/IMG_8851.jpg?raw=1" 
+            src={slide.url} 
             className="rounded-2xl"
             />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://www.dropbox.com/s/k2gppwmyt7bnjpl/IMG_8835.jpg?raw=1"
-          className="rounded-2xl"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://www.dropbox.com/s/t1qpiys4f86v8jf/IMG_8822.jpg?raw=1"
-          className="rounded-2xl"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://www.dropbox.com/s/dso3k1l0ttnmc02/IMG_8829.jpg?raw=1"
-          className="rounded-2xl"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://www.dropbox.com/s/dso3k1l0ttnmc02/IMG_8829.jpg?raw=1"
-          className="rounded-2xl"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://www.dropbox.com/s/dso3k1l0ttnmc02/IMG_8829.jpg?raw=1"
-          className="rounded-2xl"
-          />
-        </SwiperSlide>
+        )}
       </Swiper>
     </>
   );
---
+++ app/lib/swiperSlides.ts
@@ -0,0 +1,22 @@
+const swiperSlides = [
+  {
+    url: 'https://www.dropbox.com/s/3u5g0h3d6sh86xu/IMG_8851.jpg?raw=1'
+  },
+  {
+    url: 'https://www.dropbox.com/s/vpokcbaqyh8oz36/IMG_8585.jpg?raw=1'
+  },
+  {
+    url: 'https://www.dropbox.com/s/dso3k1l0ttnmc02/IMG_8829.jpg?raw=1'
+  },
+  {
+    url: 'https://www.dropbox.com/s/k2gppwmyt7bnjpl/IMG_8835.jpg?raw=1'
+  },
+  {
+    url: 'https://www.dropbox.com/s/dmmo6r34l48ladx/IMG_8591.jpg?raw=1'
+  },
+  {
+    url: 'https://www.dropbox.com/s/t1qpiys4f86v8jf/IMG_8822.jpg?raw=1'
+  },
+]
+
+export default swiperSlides
