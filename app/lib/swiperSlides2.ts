869ce92849d879047987bf21d009c694a61b60f5
y1ungspace <y1ung.space@gmail.com>
2023-08-07 14:59:52 +0200

    fix image loading in Swiper2

--- app/lib/swiperSlides2.ts
+++ app/lib/swiperSlides2.ts
@@ -1,38 +1,38 @@
 const swiperSlides2 = [
   {
-    url: "https://www.dropbox.com/s/babf0aak90im5si/Example8.jpg?raw=1",
+    url: "/images/Example8.jpg",
     progress: 1,
   },
   {
-    url: "https://www.dropbox.com/s/b8sfvabi2p9mgf0/Example1.jpg?raw=1",
+    url: "/images/Example1.jpg",
     progress: 2,
   },
   {
-    url: "https://www.dropbox.com/s/wrx1fpyf5oehqf3/Example6.jpg?raw=1",
+    url: "/images/Example6.jpg",
     progress: 0,
   },
   {
-    url: "https://www.dropbox.com/s/wytcc1ph3vxo5r8/Example4.jpg?raw=1",
+    url: "/images/Example4.jpg",
     progress: 1,
   },
   {
-    url: "https://www.dropbox.com/s/x3yqwtcfo13b4n2/Example7.jpg?raw=1",
+    url: "/images/Example7.jpg",
     progress: 2,
   },
   {
-    url: "https://www.dropbox.com/s/y8qa70khc28as1a/11%20copie.jpg?raw=1",
+    url: "/images/Example9.jpg",
     progress: 0,
   },
   {
-    url: "https://www.dropbox.com/s/1xe1znm16ptif2w/Example2.jpg?raw=1",
+    url: "/images/Example2.jpg",
     progress: 1,
   },
   {
-    url: "https://www.dropbox.com/s/m1cwuo836kw90si/Example3.jpg?raw=1",
+    url: "/images/Example3.jpg",
     progress: 2,
   },
   {
-    url: "https://www.dropbox.com/s/g6ej9a6az96jw71/Example5.jpg?raw=1",
+    url: "/images/Example5.jpg",
     progress: 0,
   },
 ]
