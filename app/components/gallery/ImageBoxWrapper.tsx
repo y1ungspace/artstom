145e3c72b416b8b277d8c89802104e32611a1d1d
y1ungspace <y1ung.space@gmail.com>
2023-08-25 16:43:37 +0200

    add all blocks to Gallery page

--- app/components/gallery/ImageBoxWrapper.tsx
+++ app/components/gallery/ImageBoxWrapper.tsx
@@ -3,7 +3,7 @@
 import { IImageBox } from "@/app/variables/Interfaces"
 import ImageBox from "./ImageBox"
 
-const imageBox1:IImageBox = {
+const imageBox2:IImageBox = {
   name: 'Подуст' ,
   serviceType:  'УСЛУГА',
   id: 1,
@@ -22,6 +22,42 @@ const imageBox1:IImageBox = {
   ],
 }
 
+const imageBox1:IImageBox = {
+  name: 'Шевелёва' ,
+  serviceType:  'УСЛУГА',
+  id: 2,
+  images: [
+    'https://www.dropbox.com/scl/fi/fc9502oy5jrofr65lbo71/2023-06-29-200459.jpg?rlkey=54be1qt5847gnz8r726apn5c4&raw=1',
+    'https://www.dropbox.com/scl/fi/9pvk7prj9xm8gwyliozbm/IMG_E6368.JPG?rlkey=ey16nen22c8uqnkah816agrqz&raw=1',
+    'https://www.dropbox.com/scl/fi/x08zwjp2e2ac76fsaoz3k/DSC_0758.jpg?rlkey=ku6iye0dfephvfafak2lisvay&raw=1',
+    'https://www.dropbox.com/scl/fi/oxp3ba3ydnz4wwohstkbw/DSC_0691.jpg?rlkey=uj9ufyz9q2fd07j3h8cxompzs&raw=1',
+    'https://www.dropbox.com/scl/fi/icy86ran7rimfak9grpta/DSC_0688.jpg?rlkey=yttv2xv9ek75tqjc6mg3bmwh8&raw=1',
+    'https://www.dropbox.com/scl/fi/wt8xshm4whcs1npxrtff5/DSC_0676.jpg?rlkey=v5u6lcrjv74flo53oz6lvidtj&raw=1',
+    'https://www.dropbox.com/scl/fi/yjhv50a5t2wuhem8vvioa/DSC_0782.jpg?rlkey=2tax205lqk4tfukaj0u9m0nbj&raw=1',
+    'https://www.dropbox.com/scl/fi/4kbppjatc2mwa9fd0glme/DSC_0694.jpg?rlkey=yqiifcc4ppg7iv7vfmb3h5cyq&raw=1',
+    'https://www.dropbox.com/scl/fi/td4n31nhcdvg8di7wzjz8/DSC_0681.jpg?rlkey=yaq49vf5g86kfvw7o9qfao56e&raw=1',
+    'https://www.dropbox.com/scl/fi/v951hon50nxgmw0b49tl7/DSC_0697.jpg?rlkey=447yu5zyqumqlwurob5zuw8zn&raw=1',
+    'https://www.dropbox.com/scl/fi/wimb8gaewax99zdlji3ue/DSC_0696.jpg?rlkey=nqk7khrx0k6jl1akvuy0vabvf&raw=1',
+    'https://www.dropbox.com/scl/fi/8gylyakb1x1t4asqynbgq/DSC_0792.jpg?rlkey=59k6cgcsf9tvo6z1vcj0488a4&raw=1',
+    'https://www.dropbox.com/scl/fi/ajmfqr6jicls8qzhhkqam/DSC_0763.jpg?rlkey=9uz4urafzj8b3fmv1q3xq5lte&raw=1',
+    'https://www.dropbox.com/scl/fi/3q0nsf12v8zyowx5ewi49/DSC_0677.jpg?rlkey=53li8zbfkx09t2rbqer03ctsm&raw=1',
+    'https://www.dropbox.com/scl/fi/imxvyltojbl2pb7veszf6/DSC_0761.jpg?rlkey=oum5mgjzaqev86vv4jd2lgx3d&raw=1',
+    'https://www.dropbox.com/scl/fi/0hm6snbn5s9vqtwlmrn8e/DSC_0698.jpg?rlkey=1c497yljtx3dlajy5sv7aiqoq&raw=1',
+    'https://www.dropbox.com/scl/fi/ve2km27zzww3ixuvad2re/DSC_0705.jpg?rlkey=na8fjoewb9u0kiizztl1jb49t&raw=1',
+    'https://www.dropbox.com/scl/fi/n8ucm4hf4a9bbphvhf1gr/DSC_0795.jpg?rlkey=27ll6oh5xe9p0tmrs6n7ylxhn&raw=1',
+    'https://www.dropbox.com/scl/fi/78xisht51hdtq16fcgttp/DSC_0780.jpg?rlkey=guckuhwxcgqvv8a4vgf2kfytt&raw=1',
+    'https://www.dropbox.com/scl/fi/1tpfd32cn9el0pwbvrmeg/DSC_0778.jpg?rlkey=y8d0x5a6htadnhxicr5zu0i63&raw=1',
+    'https://www.dropbox.com/scl/fi/3ls6es6tco0fftvi5jkqn/DSC_0767.jpg?rlkey=7kz6w8eh851hgvpi3izjofxw8&raw=1',
+    'https://www.dropbox.com/scl/fi/smfob1mm76um7hfm5crhc/DSC_0752.jpg?rlkey=qvufr90pq3b3tb5iky8prrsjc&raw=1',
+    'https://www.dropbox.com/scl/fi/u4w4vlynt4r6cef9ebvqz/DSC_0709.jpg?rlkey=4b7x80u2wpbefl72691q0txvl&raw=1',
+    'https://www.dropbox.com/scl/fi/5gcxkv537o3mmybujv9fe/DSC_0701.jpg?rlkey=whnltwn9nnqg1g64irajwn9tf&raw=1',
+    'https://www.dropbox.com/scl/fi/7xgxk79nsx83gccvj0o8b/DSC_0747.jpg?rlkey=9yd4x5y9y7cl39570r29zqhjx&raw=1',
+    'https://www.dropbox.com/scl/fi/guo8ojm0irz38smderk5w/DSC_0679.jpg?rlkey=gdjs1euim53eqi2voayetuzpd&raw=1',
+    'https://www.dropbox.com/scl/fi/whksbkf8mg70q5t6yeykl/DSC_0777.jpg?rlkey=9k2wygkzd9dux91yn2afdwvlq&raw=1',
+    'https://www.dropbox.com/scl/fi/pyxzbbf618bvco5th0j0n/DSC_0680.jpg?rlkey=aexrvb420csfz9n01q8mvhiy7&raw=1',
+    'https://www.dropbox.com/scl/fi/xvqo6087r8yftcee5gynm/IMG_6443.MOV?rlkey=mqy2t8wo5prs65raggr2m3kn3&raw=1'
+  ],
+}
 
 const ImageBoxWrapper: React.FC = () => {
   return(
@@ -30,7 +66,7 @@ const ImageBoxWrapper: React.FC = () => {
         mt-16
       ">
        <ImageBox props={imageBox1} />
-       {/* <ImageBox images={images2} /> */}
+       <ImageBox props={imageBox2} />
     </section>
   )
 }
